72740c16bd8e6516a4f00ed5955e9e1359889bcd
Evgenii Morozov <zheka1101@yandex.ru>
2021-07-15 14:58:00 +0500

    Codewars solution

--- Python/5_kuy/string_incrementer.py
+++ Python/5_kuy/string_incrementer.py
@@ -3,24 +3,37 @@
 import re
 
 
-def increment_string(string: str) -> str:
-    if string == "":
-        return "1"
+import re
 
-    if string.isalpha():
-        return string + "1"
 
-    nums = re.findall(r"^[a-z]*(0*\d*)", string, flags=re.IGNORECASE)[0]
-    alpha = re.findall(r"^([a-z]*)0*\d*", string, flags=re.IGNORECASE)[0]
+def increment_string(s):
+    number = re.findall(r'\d+', s)
+    if number:
+        s_number = number[-1]
+        s = s.rsplit(s_number, 1)[0]
+        number = str(int(s_number) + 1)
+        return s + '0' * (len(s_number) - len(number)) + number
+    return s + '1'
 
-    nums_result = str(int(nums) + 1)
 
-    if len(nums) < len(nums_result):
-        while len(nums) != len(nums_result):
-            nums_result.split().extend(0, "0")
-        nums_result = "".join(nums_result)
-
-    return alpha + nums_result
+# def increment_string(strng: str) -> str:
+#     if strng == "":
+#         return "1"
+#
+#     if string.isalpha():
+#         return string + "1"
+#
+#     nums = re.findall(r"^[a-z]*(0*\d*)", strng, flags=re.IGNORECASE)[0]
+#     alpha = re.findall(r"^([a-z]*)0*\d*", strng, flags=re.IGNORECASE)[0]
+#
+#     nums_result = str(int(nums) + 1)
+#
+#     if len(nums) < len(nums_result):
+#         while len(nums) != len(nums_result):
+#             nums_result.split().extend(0, "0")
+#         nums_result = "".join(nums_result)
+#
+#     return alpha + nums_result
 
 
 if __name__ == "__main__":
