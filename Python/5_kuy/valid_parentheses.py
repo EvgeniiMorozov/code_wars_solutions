abf1680897092642447f52d6b56f4088025998cb
Evgenii Morozov <zheka1101@yandex.ru>
2021-07-16 13:46:22 +0500

    Valid Parentheses

---
+++ Python/5_kuy/valid_parentheses.py
@@ -0,0 +1,22 @@
+# https://www.codewars.com/kata/52774a314c2333f0a7000688
+
+def valid_parentheses(string):
+    count = 0
+    for char in string:
+        if char == "(":
+            count += 1
+        elif char == ")":
+            count -= 1
+        if count < 0:
+            return False
+    return count == 0
+
+if __name__ == "__main__":
+    arr = [
+        "()",
+        ")(()))",
+        "(",
+        "(())((()())())"
+    ]
+    for chunk in arr:
+        print(valid_parentheses(chunk))
