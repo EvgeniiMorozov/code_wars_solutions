748b26d5dad38b9247407aa331f03e59c76f348b
EvgeniiMorozov <emorozov1101@gmail.com>
2021-07-16 18:35:04 +0500

    Permutations

--- Python/4_kuy/permutations.py
+++ Python/4_kuy/permutations.py
@@ -12,5 +12,22 @@ def permutations(string):
     return list(result)
 
 
+"""
+import itertools
+
+def permutations(string):
+    return list("".join(p) for p in set(itertools.permutations(string)))
+
+
+def permutations(s):
+    if len(s) == 0:
+        return []
+    elif len(s) == 1:
+        return [s]
+    else:
+        return set(s[i]+p for i in range(len(s)) for p in permutations(s[:i] + s[i+1:]))
+"""
+
+
 if __name__ == "__main__":
     print(permutations("aabb"))
