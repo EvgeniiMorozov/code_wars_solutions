a7a9f6cf1ca87f8bc849b55fabd2cb9873d1627b
EvgeniiMorozov <emorozov1101@gmail.com>
2021-07-17 20:20:46 +0500

    codewars solutions

---
+++ Python/6_kuy/valid_string.py
@@ -0,0 +1,12 @@
+# https://www.codewars.com/kata/52f3bb2095d6bfeac2002196/train/python
+
+def valid_word(seq, word):
+    # your code here
+    pass
+
+
+"""
+def valid_word(seq, word):
+    return all(word.count(chunk) for chunk in seq)
+"""
+
