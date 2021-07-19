5d6c63853e6765ea628e313c94df69a32da4d543
Evgenii Morozov <zheka1101@yandex.ru>
2021-07-19 15:02:22 +0500

    codewars solution

--- SQL/8_kuy/on_the_canadian_border.sql
+++ SQL/8_kuy/on_the_canadian_border.sql
@@ -3,3 +3,5 @@ FROM travelers
 WHERE country != "Canada"
     and country != "Mexico"
     and country != "USA";
+
+-- SELECT name, country FROM travelers WHERE country NOT IN ('Canada', 'Mexico', 'USA');
