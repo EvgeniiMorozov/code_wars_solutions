8d5a4bf353271ed37cd06d8592f2f0222f54c493
EvgeniiMorozov <emorozov1101@gmail.com>
2022-03-17 20:19:23 +0500

    Added "sum_of_pairs" solvation

--- Python/5_kuy/sum_of_pairs.py
+++ Python/5_kuy/sum_of_pairs.py
@@ -1,19 +1,40 @@
 def sum_pairs(ints: list[int], s: int):
-    arr = []
-    for i in range(len(ints)):
-        term = s - ints[i]
-        if term == 0:
-            continue
-        if term not in ints:
-            continue
+    results = dict()
+    short_ints = simplify(ints)
+    for idx_a, a in enumerate(short_ints):
+        for idx_b, b in enumerate(short_ints):
+            if idx_a != idx_b:
+                if a + b == s:
+                    diff = abs(idx_a - idx_b)
+                    if diff == 1:
+                        return [a, b]
 
-        idx = ints.index(term)
-        arr.append((i, idx, abs(idx - i)))
-    print(arr)
+                    if diff not in results:
+                        results[diff] = [a, b]
 
-    return None
+    return results[min(results)] if len(results) > 0 else None
+
+
+def simplify(ints: list) -> list:
+    result = []
+    temp = -1
+    for i in ints:
+        if temp != i:
+            temp = i
+            result.append(i)
+
+    return result
 
 
 if __name__ == "__main__":
     print(sum_pairs([10, 5, 2, 3, 7, 5], 10))
     print(sum_pairs([5, 9, 13, -3], 10))
+
+"""
+def sum_pairs(lst, s):
+    cache = set()
+    for i in lst:
+        if s - i in cache:
+            return [s - i, i]
+        cache.add(i)
+"""
